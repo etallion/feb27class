e78c1daa6eeeaa2f8aebb39a157675b83df2f68f
Will Mangimelli <willwinman@gmail.com>
2019-02-27 22:52:26 -0600

    another name change

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+    <h1> Wednesday night classes rocks! </h1> <img href="">
+</header>
+    <h2>William </h2>
+
+    <div> <img src="https://media.licdn.com/dms/image/C4E03AQGUa65hRs7sjQ/profile-displayphoto-shrink_200_200/0?e=1556755200&v=beta&t=3TJhSeEi1LvTtD8W7-y7xUh0xUqKNpf4qD-psOeO_oo" alt="man of EVERY year" > </div>
+    <p> I am a software developer by night and superdad by day.</p>
+<ul>
+    <li><strong>Email <a href="mailto:willwinman@gmail.com">willwinman@gmail.com</a></strong></li>
+    <li><strong>Github <a href="www.github.com">http://github.com/</a></strong></li>
+    <li><strong>Portfolio &nbsp; &nbsp; <a href="blog.wilakers.com">Wilakers Wanders</a></strong></li> 
+</ul>
+
+</body>
+</html>
